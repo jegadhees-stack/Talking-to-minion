323627f431de49af2f29ead532057b21a7e927f7
jegadhees-stack <jegadhees970@gmail.com>
2022-07-13 02:58:17 +0530

    output correction

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var outputDiv = document.querySelector("#output");
 
 function clickHandler(){
     console.log("clicked");
-    outputDiv.innerText = txtInput.value;
+    outputDiv.innerText = "mindijsfuyva " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
