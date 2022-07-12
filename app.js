e918439e28683bbd73b29c2f4321e93297913f49
jegadhees-stack <jegadhees970@gmail.com>
2022-07-13 01:58:30 +0530

    output

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler()
-{
+outputDiv.innertext = "tanyapratap";
+
+function clickHandler(){
     outputDiv.innertext = "kasijidbijbdv" + txtInput.value;
-}
+};
+
 btnTranslate.addEventListener("click", clickHandler)
