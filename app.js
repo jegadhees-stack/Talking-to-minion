55b1810daea3e1e00790e1a740aebb43535ce4af
jegadhees-stack <jegadhees970@gmail.com>
2022-07-13 15:39:25 +0530

    repair

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
-    return serverURL + "?" +"text=" + input;
-};
+    return serverURL + "?" +"text=" + input
+}
 
 function errorHandler(error) {
     console.log("error occured!!", error);
     alert("something wrong with this, please try again later")
-};
+}
 
 function clickHandler() {
     var inputText = txtInput.value;
@@ -22,7 +22,7 @@ function clickHandler() {
             var translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
            })
-    .catch(errorHandler)
+        .catch(errorHandler)
    
 };
 
