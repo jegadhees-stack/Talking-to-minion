3c8da8fbd9ac0553d8254ba46c49565ca8cd7f03
jegadhees-stack <jegadhees970@gmail.com>
2022-07-13 12:15:23 +0530

    Final output

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/"
+
+function getTranslationURL(input)
+{
+    return serverURL = "?" +"text=" + input;
+}
+
+function errorHandler(error)
+{
+    console.log("error occured!!");
+    alert("something wrong with this, please try again later");
+}
+
+
+
+
 function clickHandler(){
-    console.log("clicked");
-    outputDiv.innerText = "mindijsfuyva " + txtInput.value;
-};
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(responce => responce.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+
+    
+}
 
 btnTranslate.addEventListener("click", clickHandler)
